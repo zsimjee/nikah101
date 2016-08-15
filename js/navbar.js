6d08489ed4912c92eb2eb9ab2f848900d36c8b57
Simjee <zssimjee@amazon.com>
2016-08-14 20:22:29 -0700

    basic pages

---
+++ js/navbar.js
@@ -0,0 +1,5 @@
+
+document.getElementById("navbar").innerHTML="<a id='home_page' class='navlink' href=index.html>Home Page</a>\n\
+ <a id='about' class='navlink' href=about.html>About</a> \n\
+<a id='faq' class='navlink' href=faq.html>FAQ</a>";
+
