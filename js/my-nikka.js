9c4864bf737c7fb0abe23a54b835679c1cd04364
Simjee <zssimjee@amazon.com>
2016-08-14 21:34:27 -0700

    basic bootstrap

---
+++ js/my-nikka.js
@@ -0,0 +1,3 @@
+$("#body").ready(function() {
+	$("#navbar").html("<ul class='nav navbar-nav'><li><a href='index.html'>Home</a></li><li><a href='about.html'>About</a></li><li><a href='faq.html'>FAQ</a></li></ul>");
+});
