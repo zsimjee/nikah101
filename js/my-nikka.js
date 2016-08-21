dc0a1cf061b99713ef9c250dbfa502ffcfc5213b
Simjee <zssimjee@amazon.com>
2016-08-20 22:30:41 -0700

    completed bootstrap redesign

--- js/my-nikka.js
+++ js/my-nikka.js
@@ -1,3 +1,3 @@
 $("#body").ready(function() {
-	$("#navbar").html("<ul class='nav navbar-nav'><li><a href='index.html'>Home</a></li><li><a href='about.html'>About</a></li><li><a href='faq.html'>FAQ</a></li></ul>");
+	$("#navbar").html("<ul class='nav navbar-nav'><li><a class='navbar-brand' href='index.html'>My Nikka</a></li><li><a href='about.html'>About</a></li><li><a href='faq.html'>FAQ</a></li></ul>");
 });
