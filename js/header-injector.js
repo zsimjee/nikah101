a2ae3cc470b62e4d14d97df048762b3ab1caea10
Nezar Eltal <nezar.eltal@gmail.com>
2018-09-28 18:04:20 -0700

    Updated Github with September 2018 aws bucket files

---
+++ js/header-injector.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    //populate info pages
+    if (getCurrentPage().startsWith("getting_married.html") || getCurrentPage().startsWith("pre_marriage.html") || getCurrentPage().startsWith("married_life.html")) {
+        $('body').html("<div id='header'></div><div id='content_container' class='container beneath_nav' style='margin-bottom:30px;'/><div id='footer'></div>");
+        
+        var toFetch = "";
+        if (getCurrentPage().startsWith("getting_married.html"))
+            toFetch = "Getting Married";
+        else if (getCurrentPage().startsWith("pre_marriage.html"))
+            toFetch = "Pre-Marriage"
+        else if (getCurrentPage().startsWith("married_life.html"))
+            toFetch = "Married Life"
+
+        populate_page(getWebsite(), toFetch);
+    }
+
+    $('#header').load("header.html", function() {
+        findNavItemActive("index");
+        findNavItemActive("pre_marriage");
+        findNavItemActive("getting_married");
+        findNavItemActive("married_life");
+        findNavItemActive("testimonials");
+        findNavItemActive("faq");
+    });
+
+    $('#footer').load("footer.html");
+});
+
+function findNavItemActive(id) {
+    var lookup = id + ".html";
+    var className = window.location.href.includes(lookup) ? "active" : "";
+    var elemLookup = "#" + id;
+    $(elemLookup).addClass(className);
+}
+
+function getCurrentPage() {
+    var uri = window.location.href;
+    return uri.substring(uri.lastIndexOf("/") + 1);
+}
--- js/my-nikka.js
+++ js/my-nikka.js
@@ -1,3 +1,3 @@
-$("#body").ready(function() {
-	$("#navbar").html("<ul class='nav navbar-nav'><li><a href='index.html'>Home</a></li><li><a href='about.html'>About</a></li><li><a href='faq.html'>FAQ</a></li></ul>");
+$(document).ready(function(){
+    $('body').css('background-image', 'url(img/dust_scratches.png)');
 });
