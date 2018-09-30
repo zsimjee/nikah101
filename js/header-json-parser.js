f271ed00845788c504d9b3626e4822f7c097d6e5
Nezar Eltal <nezar.eltal@gmail.com>
2018-09-29 18:07:55 -0700

    Centered titles, removed navbar portions on index page, added favicon

--- js/header-json-parser.js
+++ js/header-json-parser.js
@@ -3,16 +3,16 @@ function populate_page(page_json, target_heading) {
 	for (var heading_num in page_json.headings) {
 		var _heading = page_json.headings[heading_num];
 		if (_heading.title == target_heading) {
-			
-			content_dom += "<div class='container'><div class='row'>" 
-							+ "<h1>" + _heading.title + "</h1>"
+
+			content_dom += "<div class='container'><div class='row'>"
+							+ "<h1 style='text-align:center'>"+ _heading.title + "</h1>"
 							+ "<p>" + _heading.description + "</p>"
 						+ "</div><div class='row'>";
 
 			for (var phase_num in _heading.phases) {
 				_phase = _heading.phases[phase_num];
 
-				content_dom += "<div><h2>"
+				content_dom += "<div><h2 class='text-center'>"
 								+ _phase.phase
 								+ "<br><em>" + _phase.description + "</em>"
 							+ "</h2></div>";
@@ -29,7 +29,7 @@ function populate_page(page_json, target_heading) {
 					var stripe = true;
 					for(var link_num in _subheading.links) {
 						_link = _subheading.links[link_num];
-						link_id = phase_num + "_" + subheading_num + "_" + link_num; 
+						link_id = phase_num + "_" + subheading_num + "_" + link_num;
 
 						if (_link.link.includes("youtube.com")) {
 							content_dom += "<div class='container link_section " + (stripe ? "stripe" : "") + "'>"
