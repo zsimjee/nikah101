96b1d18c6f756d79efe8d6f411c5e08528554e2f
Nezar Eltal <nezar.eltal@gmail.com>
2018-10-31 14:16:48 -0700

    Search is complete

--- js/search-data.js
+++ js/search-data.js
@@ -3,7 +3,7 @@ var query = getSearchQuery().toLowerCase();
 
 function getSearchResults(){
   var result = {"headings": new Array()};
-  var search_heading = {"title": "Search", "description": "Results for your query: <mark>"+getSearchQuery()+"</mark>",
+  var search_heading = {"title": "Search", "description": "<h1> - Results for your query: <mark><b>"+getSearchQuery()+"</b></mark><br><br></h1>",
                         "phases": new Array()}
   for (var heading_num in site.headings){
     var temp = getHeadingData(site.headings[heading_num]);
@@ -11,7 +11,7 @@ function getSearchResults(){
         search_heading.phases.push(temp[each_result]);
   }
   result.headings.push(search_heading);
-  return result;
+  return highlightSearchTerms(result);
 
 }
 
@@ -32,6 +32,11 @@ function getPhaseData(heading_title, phase){
   phase_result.phase = heading_title + " - " + phase.phase;
   phase_result.description = phase.description;
   phase_result.subheadings = new Array();
+
+  if (phase.description.toLowerCase().includes(query)){
+    phase_result.subheadings = phase.subheadings;
+    return phase_result;
+  }
   for (var subheading_num in phase.subheadings){
     var temp = getSubheadingData(heading_title, phase.subheadings[subheading_num]);
     if (temp.links.length != 0){
@@ -72,4 +77,40 @@ function getSearchQuery(){
   currentPage.substring(currentPage.lastIndexOf("/") + 1);
   return decodeURIComponent(currentPage.substring(currentPage.lastIndexOf("=")+1));
 
+}
+
+function highlightSearchTerms(search_json){
+  if (search_json.headings.length == 0)
+      return search_json;
+
+  for (var p_num in search_json.headings[0].phases){
+    search_json.headings[0].phases[p_num].description = addHighlight(search_json.headings[0].phases[p_num].description);
+    for (var sub_num in search_json.headings[0].phases[p_num].subheadings){
+      search_json.headings[0].phases[p_num].subheadings[sub_num].subheading = addHighlight(search_json.headings[0].phases[p_num].subheadings[sub_num].subheading);
+      search_json.headings[0].phases[p_num].subheadings[sub_num].subheading_description = addHighlight(search_json.headings[0].phases[p_num].subheadings[sub_num].subheading_description);
+      for (var l_num in search_json.headings[0].phases[p_num].subheadings[sub_num].links){
+        search_json.headings[0].phases[p_num].subheadings[sub_num].links[l_num].name = addHighlight(search_json.headings[0].phases[p_num].subheadings[sub_num].links[l_num].name);
+        search_json.headings[0].phases[p_num].subheadings[sub_num].links[l_num].description = addHighlight(search_json.headings[0].phases[p_num].subheadings[sub_num].links[l_num].description);
+      }
+    }
+
+
+  }
+  return search_json;
+}
+
+function addHighlight(source){
+  var result = "";
+  var i = 0;
+  while (i < source.length){
+    if (source.substring(i, i+query.length).toLowerCase() == query){
+      result += "<span style='background-color: yellow;'>" + source.substring(i, i+query.length) + "</span>";
+      i += query.length
+    }
+    else{
+      result += source.substring(i, i+1);
+      ++i;
+    }
+  }
+  return result;
 }
